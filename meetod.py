3a7e22902ca6d36498b86a26963d48559e55d981
klesment <124093579+klesment@users.noreply.github.com>
2025-06-06 13:10:31 +0300

    Update meetod.py

--- meetod.py
+++ meetod.py
@@ -1,32 +1,34 @@
 import streamlit as st
 
-st.header('Sündimus rahvastikuprognoosis')
+st.header('Sündimuse mõju tuleviku rahvastiku suurusele ja vanuskoostisele')
 st.markdown('''
-Rahvastikuprognoosides kasutatav kohort-komponent meetod on deterministlik viis rahvastiku vanuselise koosseisu ja suuruse prognoosimiseks teadaolevate ja 
-eeldatud andmete põhjal. Selle aluseks on tõedemus, et inimesi tekib juurde ainult sündides või sisse rännates, 
+Rahvastikuprognoosides kasutatav kohort-komponent meetod on deterministlik viis rahvastiku vanuselise koosseisu ja suuruse prognoosimiseks teadaolevate 
+andmete ja tehtavate eelduste põhjal. Selle aluseks on tõedemus, et inimesi tekib juurde ainult sündides või sisse rännates, 
 neid jääb vähemaks surres või välja rännates. Teades rahvastiku suurust ja vanuselist koosseisu mingil aastal, saame 
 prognoosida rahvastiku suurust ja vanuselist koosseisu järgnevatel aastatel, vastavalt sellele, kuidas eeldatakse sündimuse, 
-suremus ja rände muutust muutuva vanusstruktuuri juures. 
+suremus ja rände muutust järgnevatel aastatel. 
 
-Arusaadavalt tuleviku sündimus, rändevood ja suremus ei ole teada, mistõttu on prognoosimiseks vaja nende kohta teha eeldusi. 
+Arusaadavalt tuleviku sündimus,  suremus ja rändevood ei ole teada. Prognoosimiseks on vaja nende kohta teha eeldusi ehk oletada, mis suunas 
+            vastavad arengud võiksid toimuda. 
 
-Käesolev prognoosimudel võimaldab teatud piirides sündimust puudutavate eeldustega manipuleerida. Näiteks anda prognoosimiseks ette:
+Käesolev prognoosimudel keskendub ainult sündimuse komponendile. 
+Mudel võimaldab teatud piirides sündimuse muutust puudutavate eeldustega manipuleerida. Näiteks anda prognoosimiseks ette:
 
 - perioodsündimuse langus/tõus prognoositava perioodi jooksul
 - sündimustaseme muutumise kiirus, st kas muutus toimub kiiresti või aeglaselt
 - ema keskmise sünnivanuse muutus, nt et see jätkab tõusu. 
 
 Lihtsuse huvides on muud rahvastiku koostist mõjutavad komponendid prognoosist välja jäetud. St, ei eeldata suremuse muutust ja 
-oodatava eluea tõusu, kuigi see oleks loogiline. Teiseks, sisse- ja väljaränne, mis võiks omakorda vanuskoostist mõjutada, 
+oodatava eluea tõusu, kuigi see oleks loogiline eeldus. Teiseks, sisse- ja väljaränne, mis võiks omakorda vanuskoostist mõjutada, 
 ei ole arvesse võetud. 
 
-### Arvutus 
+### Prognoosimismeetod 
 Üks võimalus prgnoosimise tarvis rahvastikumuutuste aastast-aastasse edasikandmiseks on nn Leslie maatriks. 
 ''')
 
 ### Leslie maatriks            
 st.markdown('''
-            Leslie maatriks (https://en.wikipedia.org/wiki/Leslie_matrix) on niisugune maatriks, mille ridade arv vastab 
+            Leslie maatriks (https://en.wikipedia.org/wiki/Leslie_matrix) on maatriks, mille ridade arv vastab 
             kasutatavatele vanuserühmade arvule. Maatriksi esimesele reale kantakse vanuspõhiseid sündimuskordajaid (sündide arv vanusrühmas 
             oleva naisrahvastiku kohta) ja nn alamdiagonaalile iga vanusrühma ellujäämise tõenäosus (elutabeli $L_x$). 
             Ülejäänud elemendid on nullid. 
@@ -89,14 +91,14 @@ x_t = \mathbf{L}^t x_0
 st.markdown('''
             seejuures $x_0$ on prognoosi aluseks oleva aasta rahvastiku vanusejaotus. 
             See aga tähendab, et prognoositava perioodi jooksul ei muutu sündimuskordajad, mis ei ole tõenäoliselt realistlik.
-                  Antud juhul kasutame siiski korduvaid ühe aasta prognoose, kuna maatriksis olevad sündimuskordajad muutuvad igal aastal. 
+                  Antud juhul kasutame järjestikuseid ühe aasta prognoose nii, et sündimuskordajad muutuvad igal aastal. 
                   See tähendab, et igal aastal arvutatakse uus Leslie maatriks, mis vastab selle aasta eeldatavatele sündimuskordajatele ja 
-                  ellujäämise tõenäosustele. See võimaldab prognoosida rahvastiku muutusi dünaamilisemalt ja realistlikumalt.
+                  ellujäämise tõenäosustele. See võimaldab prognoosis sündimust dünaamilisemalt käsitleda.
                   ''')
 
-st.markdown('''Seejuures tekib probleem, et summmaarse sündimuskordaja (TFR) asemel on Leslie maatriksis
-            vanusepõhised sündimuskordajad (ASFR). TFR on ASFR-i summa üle kõigi vanusvahemike. Järelikult peame eeldatud tuleviku TFR-i
-            teisendama vanuspõhisteks sündimuskordajateks, et need sobiksid Leslie maatriksisse.  
+st.markdown('''Seejuures tekib probleem, et summmaarse sündimuskordaja (TFR) asemel on Leslie maatriksis vaja kasutada
+            vanusepõhiseid sündimuskordajaid (ASFR), samas kui TFR on ASFR-i summa üle kõigi vanusvahemike. Järelikult peame eeldatud tuleviku TFR-i
+            kõigepealt teisendama vanuspõhisteks sündimuskordajateks.  
             ''')
 
 
@@ -105,9 +107,11 @@ st.markdown('''Seejuures tekib probleem, et summmaarse sündimuskordaja (TFR) as
 st.write('''
 Vanusepõhiste sündimuskordajate modelleerimiseks kasutame nn Gamma-mudelit. Sellel on kolm parameetrit:
 - summaarne sündimuskordaja (TFR)
-- Keskmine sünnituseas (MAB)
-- Keskmise sünnitusea standardhälve (sdMAB)
-''')
+- Keskmine sünnitusvanus (MAB)
+- Keskmise sünnitusvanuse standardhälve (sdMAB)
+'''
+         
+Gamma-mudel ja selle parameetrid on defineeritud järgmiselt:)
 
 st.latex(r"""
          \Large{     
@@ -125,7 +129,7 @@ st.latex(r"\alpha_1 = TFR")
 st.latex(r"\alpha_2 = \frac{MAB}{sdMAB^2}")
 st.latex(r"\alpha_3 = \left(\frac{MAB}{sdMAB}\right)^2")
 
-st.write("Neid kolme sisendit (TFR, MAB ja sdMAB) saab kasutaja prognoosi tegemisel määrata.")
+st.write("Nendest kahte parameetrit (TFR, MAB) saab kasutaja prognoosi tegemisel muuta.")
 
 
 st.write('''
